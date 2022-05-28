5db3b12c55d534e62e2fa11163512cf21991be2f
Romi74 <rmendelbaum@gmail.com>
2022-05-27 21:39:49 -0300

    ejercicios de practica variables python realizados
    los vuelvo a subir

---
+++ ejercicio_1.py
@@ -0,0 +1,41 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+numero_1 = 2
+numero_2 = 5
+
+print(numero_1)
+print(numero_2)
+
+
+
+# Almacenar el valor de la suma en una variable
+suma = numero_1 + numero_2
+
+# ej:
+# operacion = .....
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+print("El resultado de la suma es: ", suma)
+# Repita el procedimiento para realizar la resta
+resta = numero_1 - numero_2
+print("el resultado de la resta es:", resta)
+
+#realizo la operacion de otra forma...
+print(f"el resultado de la suma es{suma}")
+print("el resultado de la suma es {}".format(suma))
+
+print(f"el resultado de la resta es {resta}")
+print("el resultado de la resta es{}".format(resta))
